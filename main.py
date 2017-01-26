89d2b3578f72eff2a8f8c18a19a9d1cda44076a6
Perigree <holisticspiral@gmail.com>
2017-01-26 11:17:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -103,15 +103,19 @@ class AddMovie(webapp2.RequestHandler):
 
         # TODO 2
         # if the user typed nothing at all, redirect and yell at them
-
+        if (not new_movie) or (new_movie.strip() == ""):
+            error = "Please specify the movie you want to add."
+            self.redirect("/?error=" + cgi.escape(error))
 
         # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
-
+        if new_movie in terrible_movies:
+            error = "Terrible choice, you don't want to add '{0}' to your Watchlist.".format(new_movie)
+            self.redirect("/?error=" + cgi.escape(error, quote=True))
 
         # TODO 1
         # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-
+         new_movie_escaped = cgi.escape(new_movie, quote=True)
         # build response content
         new_movie_element = "<strong>" + new_movie + "</strong>"
         sentence = new_movie_element + " has been added to your Watchlist!"
