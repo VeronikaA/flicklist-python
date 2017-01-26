f0a645218616ae4ff58bc7c49bebc7bcfd2bd0cf
Perigree <holisticspiral@gmail.com>
2017-01-25 20:13:35 -0500

    Add files via upload

---
+++ flicklist-python.py
@@ -0,0 +1,121 @@
+import webapp2
+
+
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FlickList</title>
+</head>
+<body>
+    <h1>FlickList</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.flicklist.com/
+    """
+
+    def get(self):
+
+        edit_header = "<h3>Edit My Watchlist</h3>"
+
+        # a form for adding new movies
+        add_form = """
+        <form action="/add" method="post">
+            <label>
+                I want to add
+                <input type="text" name="new-movie"/>
+                to my watchlist.
+            </label>
+            <input type="submit" value="Add It"/>
+        </form>
+        """
+
+        # TODO 1
+        # Include another form so the user can "cross off" a movie from their list.
+        def post(self):
+            crossed_off_movie = self.request.get("crossed-off-movie")
+
+        # TODO 4 (Extra Credit)
+        # modify your form to use a dropdown (<select>) instead a
+        # text box (<input type="text"/>)
+        
+        cross_off_form = """
+        <p>
+            <form action="/crossoff" method="post">
+                <label>
+                    I want to cross off
+                    <select name = "old-movie">
+                        <option value="The Big Lebowski">The Big Lebowski</option>
+                        <option value="Only Lovers Left Alive">Only Lovers Left Alive</<option>
+                        <option value="Interstellar">Interstellar</option>
+                        <option value="The Wind Rises">The Wind Rises</<option>
+                        <option value="My Neighbor Totoro">My Neighbor Totoro</<option>
+                    </select>
+                    from my watchlist.
+                </label>
+                <input type="submit" value="Cross Off"/>
+            </form>
+        </p>
+        """
+
+
+        content = page_header + edit_header + add_form + page_footer
+        self.response.write(content)
+
+
+class AddMovie(webapp2.RequestHandler):
+    """ Handles requests coming in to '/add'
+        e.g. www.flicklist.com/add
+    """
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        new_movie = self.request.get("new-movie")
+
+        # build response content
+        new_movie_element = "<strong>" + new_movie + "</strong>"
+        sentence = new_movie_element + " has been added to your Watchlist!"
+
+        content = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(content)
+
+
+# TODO 2
+# Create a new RequestHandler class called CrossOffMovie, to receive and
+# handle the request from your 'cross-off' form. The user should see a message like:
+# "Star Wars has been crossed off your watchlist".
+
+class CrossOffMovie(webapp2.RequestHandler):
+    """ Handles requests coming in to '/remove'
+        e.g. www.flicklist.com/remove
+    """
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        new_movie = self.request.get("old-movie")
+
+        # build response content
+        new_movie_element = "<strike>" + new_movie + "</strike>"
+        sentence = new_movie_element + " has been crossed off your Watchlist!"
+
+        content = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(content)
+
+
+
+# TODO 3
+# Include a route for your cross-off handler, by adding another tuple to the list below.
+app = webapp2.WSGIApplication([
+    ('/', Index),
+    ('/add', AddMovie)
+    ('/crossoff', CrossOffMovie)
+], debug=True)
